a7ab5fe7615279fd185f0d2613317c26a7814a99
carlinhosalma <carlinhosalma@gmail.com>
2021-11-09 15:31:30 -0200

    components staticos

---
+++ frontend/src/components/footer/index.tsx
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light"><strong>MVP</strong> desenvolvido por <a href="https://instagram.com/carlinhosalma/" target="_blank" rel="noreferrer">JVC Clusters</a></p>
+            </div>
+        </footer>
+    );
+}
+export default Footer;
