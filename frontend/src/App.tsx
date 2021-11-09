c6c8c90457b50b7c34da16e62bc412c1ece8ffe0
carlinhosalma <carlinhosalma@gmail.com>
2021-11-09 18:13:27 -0200

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,6 @@
+import BarChart from "components/BarChart";
 import DataTable from "components/DataTable";
+import DonutChart from "components/DonutChart";
 import Footer from "components/footer";
 import NavBar from "components/navbar";
 
@@ -7,7 +9,21 @@ function App() {
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Ola mundo</h1>
+        <h1 className="text-primary">Dashboard de Gestão</h1>
+        <div className="row py-3 px-3">
+
+          <div className="col-sm-6">
+            <h5 className="text-secondary "> Parceiros</h5>
+            <BarChart />
+          </div>
+          <div className="col-sm-6 ">
+          <h5 className="text-secondary "> Modalidades</h5>
+            <DonutChart />
+          </div>
+
+        </div>
+
+        <h2 className="text-primary">Projetos de Crédito de Carbono</h2>
         <DataTable />
 
       </div>
--- frontend/src/components/navbar/index.tsx
+++ frontend/src/components/navbar/index.tsx
@@ -4,7 +4,9 @@ const NavBar = () => {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
-                    <img src={logo} alt="DevSuperior" width="420" />
+                   
+                    <h1 className="text-primary ">  <img src={logo} alt="DevSuperior" width="80"className="m-3" /> 
+                    BANCO VERDE <br /></h1>
                 </nav>
             </div>
         </div>
