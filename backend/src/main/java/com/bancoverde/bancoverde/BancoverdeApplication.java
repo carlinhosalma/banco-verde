2ca09e61b3e4b13aee0578d0ecde0ac6143f14c0
carlinhosalma <carlinhosalma@gmail.com>
2021-11-09 23:44:55 -0200

    SPRING

---
+++ backend/src/main/java/com/bancoverde/bancoverde/BancoverdeApplication.java
@@ -0,0 +1,13 @@
+package com.bancoverde.bancoverde;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoverdeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BancoverdeApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/bancoverde/bancoverde/BancoverdeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bancoverde.bancoverde;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoverdeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
