e2c07ce6716eb32ef1cc1c656c39c219c4065e44
carlinhosalma <carlinhosalma@gmail.com>
2021-11-14 14:51:56 -0200

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from "react-apexcharts";
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
@@ -10,20 +11,51 @@ type ChartData = {
 
 const DonutChart = () => {
 
-    //forma errada
-    let chartData:ChartData ={ labels:[] , series:[] };
-    //forma errada
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
+    //forma correta do chartData
+    const [chartData,SetChartData] = useState<ChartData>({ labels:[] , series:[] });
+
+    useEffect(()=>{
+
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
         .then( (response) =>{
             
             const data = response.data as SaleSum[];
             const myLabels = data.map(x => x.sellerName);
             const mySeries = data.map(x => x.sum);
 
-            chartData = { labels:myLabels , series:mySeries };
+            //Forma correta a variavel executada agora é SetcartData
+            SetChartData({ labels:myLabels , series:mySeries }) ;
+
+            //forma errada
+            //chartData = { labels:myLabels , series:mySeries };
+
+          //  console.log(chartData);
+
+        });
+
+    },[]);
+    //forma errada do chartData
+    //let chartData:ChartData ={ labels:[] , series:[] };
+
+
+    //forma errada
+  /*  axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then( (response) =>{
+            
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            //Forma correta a variavel executada agora é SetcartData
+            SetChartData({ labels:myLabels , series:mySeries }) ;
+
+            //forma errada
+            //chartData = { labels:myLabels , series:mySeries };
+
             console.log(chartData);
 
         });
+    */
 
     // dados do estáticos do modelo
    /* const mockData = {
